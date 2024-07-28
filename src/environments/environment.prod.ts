3ccf286129db91c29961f1f7d7fe7404adfa7a2f
Yhulia <yuliaaprilliani10@gmail.com>
2024-07-28 11:42:04 +0700

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiAnak:
+  'https://26a515ae-feed-4bb2-9b9f-6af70238031b-00-1e1tdl1xnn4wv.sisko.replit.dev/',
+  apiOrtu:
+  'https://2ea434a8-5641-4812-b85f-325cd5caa951-00-3tbl5y92z2bdh.pike.replit.dev/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiAnak:
+  'https://26a515ae-feed-4bb2-9b9f-6af70238031b-00-1e1tdl1xnn4wv.sisko.replit.dev/',
+  apiOrtu:
+  'https://2ea434a8-5641-4812-b85f-325cd5caa951-00-3tbl5y92z2bdh.pike.replit.dev/',
 };
 
 /*
